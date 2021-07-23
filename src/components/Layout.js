f4408e1112715139f6af2dd2c0c9a315daea01db
Jaime Albornoz <albornozsotojaime@gmail.com>
2021-07-23 10:21:09 -0400

    first commit

---
+++ src/components/Layout.js
@@ -0,0 +1,85 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import axios from 'axios';
+import React, { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+
+import List from './List';
+// snippet sfc
+// snippet nfn
+// snippet usf (Simple react snippets - Burke Holland)
+
+// REVISAR FILTRO EN LA LLAMADA A LA API
+
+const Layout = () => {
+  const [data, setData] = useState([]);
+  const mostrar10Productos = () => {
+    const response = axios.get('https://www.amiiboapi.com/api/amiibo/');
+    response.then(
+      result => {
+        const filteredData = result.data.amiibo.filter((obj, index) =>
+          index < 9
+        );
+        setData(filteredData);
+      }
+    ).catch(e => {
+      console.log('error:', e);
+    });
+  };
+  const mostrarTodo = async () => {
+    const response = await axios.get('https://www.amiiboapi.com/api/amiibo/');
+    setData(response.data.amiibo);
+  };
+  // console.log(data);
+  console.log(data);
+  return (
+    <>
+      <Container fluid>
+            <Button variant="outline-danger" onClick={mostrar10Productos}>
+              Actualizar then
+            </Button>
+        {/* <Row> */}
+          {/* <Col>
+            1 of 2
+            <div>
+              {data.map((product, index) => {
+                if (index < 5) {
+                  return <List index={index} product={product} />;
+                } else {
+                  return <div></div>;
+                }
+              })}
+              </div>
+            </Col> */}
+          {/* <Button onClick={mostrarTodo}>
+            Actualizar async/await
+          </Button> */}
+          {/* <Col>2 of 2
+          <div>
+          {data.map((product, index) => {
+            if (index >= 5) {
+              return <List index={index} product={product} />;
+            } else {
+              return <div></div>;
+            }
+          })}
+          </div>
+        </Col> */}
+        {/* </Row> */}
+        <Row xs={1} md={3} className="g-4">
+        {data.map((product, index) => (
+          <Col>
+           <List index={index} product={product} />
+           </Col>
+        ))}
+        </Row>
+        {/* <Button onClick={mostrarTodo}>
+          Actualizar async/await
+        </Button> */}
+      </Container>
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/components/Productos.js
@@ -0,0 +1,13 @@
+// snippet rfce
+import React from 'react'
+
+function Productos() {
+        
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Productos
