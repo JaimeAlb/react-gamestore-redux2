6eaa9f9a04c70ebedd1800e9b5220f3814894c1f
Jaime Albornoz <albornozsotojaime@gmail.com>
2021-07-27 23:00:39 -0400

    Layout parent of Productos

--- src/hoc/layout/Layout.js
+++ src/hoc/layout/Layout.js
@@ -3,14 +3,15 @@ import Navbar1 from '../../components/Navigation/Navbar/Navbar1';
 import Productos from '../../containers/Productos';
 import Aux1 from '../auxiliar/Aux1';
 
-const Layout = () => {
+const Layout = (props) => {
   return (
-    <>
+    <Aux1>
       <Navbar1></Navbar1>
-      {/* <Productos></Productos> */}
-    </>
+      <main>
+        {props.children}
+      </main>
+    </Aux1>
   );
 };
 
 export default Layout;
-// afasdfas
